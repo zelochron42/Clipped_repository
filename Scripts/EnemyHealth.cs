66f7522c6b3be85ef8e10483d6bedf64436b64e4
Corn <30359015+Paipopaipo-Paipo-no-Shuringan@users.noreply.github.com>
2023-11-10 10:31:34 -0500

    enemy respawn code

---
+++ Scripts/EnemyHealth.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public bool respawns = true;
+    bool dead = false;
+    [SerializeField] float maxHealth;
+    public UnityEvent<Vector2> DamageReceived;
+    public UnityEvent Death;
+    float health;
+    public void AttackDamage(Vector2 attackDirection) {
+        health -= 1f;
+        DamageReceived.Invoke(attackDirection);
+    }
+    private void Awake() {
+        health = maxHealth;
+    }
+    void Start()
+    {
+        if (respawns) {
+            respawns = false;
+            EnemyRespawner er = FindObjectOfType<EnemyRespawner>();
+            er.AddToRespawn(this);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.GetComponent<TargetedCollider>()) {
+            Vector2 direction = collision.transform.right;
+            AttackDamage(direction);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0f && !dead) {
+            dead = true;
+            Death.Invoke();
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
