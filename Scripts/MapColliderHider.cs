be177a72d48c4a18e1eca82965e89281205eed52
Corn <30359015+Paipopaipo-Paipo-no-Shuringan@users.noreply.github.com>
2024-02-21 16:02:55 -0500

    last minute stuff

--- Scripts/MapColliderHider.cs
+++ Scripts/MapColliderHider.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class MapColliderHider : MonoBehaviour
 {
+    [SerializeField] bool autoHide = true;
     [SerializeField] bool hideInEditor = true;
     private void Awake() {
+        if (!autoHide)
+            return;
         if (Application.isEditor && !hideInEditor)
             return;
         SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
