760e86c1e19f47a0faab3b1288c922b81c001dba
AhmedHAbdel <id99tk15@ubalt.edu>
2024-02-21 16:24:17 -0500

    Third person alterations and Post processing

--- Scripts/thirdperson.cs
+++ Scripts/thirdperson.cs
@@ -6,15 +6,34 @@ public class thirdperson : MonoBehaviour
 {
     public CharacterController CristataController;
     public float speed = 6f;
+    public float SmoothTurntime = 0.1f;
+    float SmoothTurnVel;
+    public Transform Cam;
 
     void Update()
     {
+        Speedup();
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical). normalized;
         if (direction.magnitude >= 0.1f)
         {
-            CristataController.Move(direction * speed * Time.deltaTime);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref SmoothTurnVel, SmoothTurntime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            CristataController.Move(moveDir * speed * Time.deltaTime);
+        }
+    }
+    void Speedup()
+    {
+        if (Input.GetKeyDown("left shift"))
+        {
+            speed += 10;
+        }
+        if (Input.GetKeyUp("left shift"))
+        {
+            speed -= 10;
         }
     }
 }
