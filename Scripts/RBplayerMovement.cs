01d9354d95b89a1f4d990fd2a78e2327fe6115c5
AhmedHAbdel <id99tk15@ubalt.edu>
2024-05-01 11:33:28 -0400

    Updates

--- Scripts/RBplayerMovement.cs
+++ Scripts/RBplayerMovement.cs
@@ -124,17 +124,17 @@ public class RBplayerMovement : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            
-        
-
-        if (grounded)
+            if (grounded)
             PlayerRB.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
 
 
         else if (!grounded)
             PlayerRB.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
         
-        }
+
+       
+        
+        } 
     }
     private void SpeedControl()
     {
