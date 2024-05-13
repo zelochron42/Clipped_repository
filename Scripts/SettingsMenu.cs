9360904a68ddd7051fa12a3353584439bf4303b6
Corn <30359015+Paipopaipo-Paipo-no-Shuringan@users.noreply.github.com>
2024-05-12 20:04:40 -0400

    hopefully this fixes the menu

--- Scripts/SettingsMenu.cs
+++ Scripts/SettingsMenu.cs
@@ -8,31 +8,39 @@ public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     Resolution[] resolutions;
+    List<Resolution> culledResolutions
     public TMPro.TMP_Dropdown resolutionDropDown;
     void Start()
     {
         resolutions = Screen.resolutions;
         resolutionDropDown.ClearOptions();
         List<string> options = new List<string>();
+        culledResolutions = new List<Resolution>();
         int currentResolutionindex = 0;
         for(int i = 0; i< resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            if (!options.Contains(option)) {
+            
+            if (!culledResolutions.Contains(resolutions[i]) {
+                culledResolutions.Add(resolutions[i]);
+                string option = resolutions[i].width + " x " + resolutions[i].height;
                 options.Add(option);
-                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
-                    currentResolutionindex = i;
-                }
+            }
+        }
+
+        for (int i = 0; i < culledResolutions.Count; i++) {
+            if (culledResolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionindex = i;
             }
         }
         resolutionDropDown.AddOptions(options);
         resolutionDropDown.value = currentResolutionindex;
         resolutionDropDown.RefreshShownValue();
 
+
     }
     public void SetResolution (int resolutionIndex)
     {
-        Resolution resolution = resolutions[resolutionIndex];
+        Resolution resolution = culledResolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
